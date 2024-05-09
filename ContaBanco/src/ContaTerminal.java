a940da8670c78c81e86a677e612cfc56d4b2b6a9
Ale_Cipriano <alessandro_cipriano@hotmail.com>
2024-05-09 00:51:27 -0300

    Acrescentando script do AboutMe como comentário

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,35 @@
 /**
  Desafio para exercitar conteúdo apresentado no módulo de Sintaxe. 
  */
+
+		/* String nome = args[0];
+			String sobrebome  = args[1];
+			int idade = Integer.valueOf(args[2]);
+			double altura = Double.valueOf(args[3]); 
+			
+			// Criando o objeto "scaner"
+			
+			Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+			
+			System.out.println("Digite o seu nome");
+			String nome = scanner.next();
+			
+			System.out.println("Digite o seu sobrenome");
+			String sobrenome = scanner.next();
+			
+			System.out.println("Digite a sua idade");
+			int idade = scanner.nextInt();
+			
+			System.out.println("Digite a sua altura");
+			double altura = scanner.nextDouble();
+			
+			
+			// Imprimindo
+			System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+			System.out.println(" Tenho " + idade + " " + " anos");
+			System.out.println("Minha altura é " + altura + " m");  */
+			
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
