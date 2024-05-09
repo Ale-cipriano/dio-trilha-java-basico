10b2eb9df8bb1e7f1dfc9b1c328083101c8edb0d
Alessandro Cipriano <136939757+Ale-cipriano@users.noreply.github.com>
2024-05-08 22:11:23 -0300

    Integração com a IDE Eclipse

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+/**
+ Desafio para exercitar conteúdo apresentado no módulo de Sintaxe. 
+ */
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+
+	}
+
+}
